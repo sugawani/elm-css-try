7331841168617f6dee05270e3f84036e211d45e8
sugawani <nekorollykk@gmail.com>
2020-11-07 02:20:20 +0900

    align-self first baseline

--- src/Main.elm
+++ src/Main.elm
@@ -550,4 +550,18 @@ baselineAlignSelf =
                 , div [] [ text "5" ]
                 ]
             ]
+        , div []
+            [ h4 [] [ text "first-baseline" ]
+            , text "1行目のベースラインに合わせる"
+            , div
+                [ A.class "flex-container"
+                , A.class "align-self-container"
+                ]
+                [ div [ A.class "align-self-first-baseline" ] [ text "1" ]
+                , div [] [ text "2" ]
+                , div [] [ text "3" ]
+                , div [] [ text "4" ]
+                , div [] [ text "5" ]
+                ]
+            ]
         ]
